59fdce8ed9099f1a65fab32b0304c6c4ca8119b9
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-07-18 14:28:41 -0300

    chore: adicionando componente botão inicial

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,1 +1,10 @@
+import { Text, View } from "react-native";
+import buttonStyle from "./Style";
 
+export function Button({ number, color }) {
+  return (
+    <View style={[buttonStyle.container, { backgroundColor: color }]}>
+      <Text style={buttonStyle.number}>{number}</Text>
+    </View>
+  );
+}
