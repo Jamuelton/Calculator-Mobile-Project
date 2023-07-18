a0510731b910b6cd1b23c49c619adb8a773380b6
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-07-18 14:26:30 -0300

    style: estilizando parte dos display

--- components/Display/Styles.js
+++ components/Display/Styles.js
@@ -1,1 +1,17 @@
+import { StyleSheet } from "react-native";
 
+const displayStyle = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    padding: 10,
+  },
+  content: {
+    color: "#fff",
+    fontSize: 40,
+  },
+});
+
+export default displayStyle;
