995ee6bb6277f36ab28bf003c094d47d662690ec
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-07-18 14:24:47 -0300

    chore: adicionando configuração inicial do display

--- components/Display/Display.js
+++ components/Display/Display.js
@@ -1,1 +1,11 @@
+import { Text, View } from "react-native";
+import displayStyle from "./Styles";
 
+export function Display({ displayContent, secontContent }) {
+  return (
+    <View style={displayStyle.container}>
+      <Text style={displayStyle.content}>{displayContent}</Text>
+      <Text style={displayStyle.content}>{secontContent}</Text>
+    </View>
+  );
+}
