10b42187aeadb481d25e1bf9c9e5038b81c4f87d
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-07-18 14:37:41 -0300

    feat: adicionando botões da calculadora

--- components/Calculator/Calculator.js
+++ components/Calculator/Calculator.js
@@ -13,7 +13,93 @@ export function Calculator() {
       <View style={calculatorStyle.display}>
         <Display displayContent={secondNum} secontContent={numberDisplay()} />
       </View>
-      <View style={calculatorStyle.keyboard}>)</View>
+      <View style={calculatorStyle.keyboard}>
+        <View style={calculatorStyle.buttonRow}>
+          <TouchableHighlight>
+            <Button number="MR" color="#95afc0" />
+          </TouchableHighlight>
+
+          <TouchableHighlight>
+            <Button number="M+" color="#95afc0" />
+          </TouchableHighlight>
+
+          <TouchableHighlight>
+            <Button number="M-" color="#95afc0" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={handleClearPress}>
+            <Button number="MC" color="#95afc0" />
+          </TouchableHighlight>
+        </View>
+        <View style={calculatorStyle.buttonRow}>
+          <TouchableHighlight onPress={() => handleNumberPress("7")}>
+            <Button number="7" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleNumberPress("8")}>
+            <Button number="8" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleNumberPress("9")}>
+            <Button number="9" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleOperation("/")}>
+            <Button number="/" color="#f0932b" />
+          </TouchableHighlight>
+        </View>
+        <View style={calculatorStyle.buttonRow}>
+          <TouchableHighlight onPress={() => handleNumberPress("4")}>
+            <Button number="4" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleNumberPress("5")}>
+            <Button number="5" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleNumberPress("6")}>
+            <Button number="6" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleOperation("*")}>
+            <Button number="x" color="#f0932b" />
+          </TouchableHighlight>
+        </View>
+        <View style={calculatorStyle.buttonRow}>
+          <TouchableHighlight onPress={() => handleNumberPress("1")}>
+            <Button number="1" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleNumberPress("2")}>
+            <Button number="2" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleNumberPress("3")}>
+            <Button number="3" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleOperation("-")}>
+            <Button number="-" color="#f0932b" />
+          </TouchableHighlight>
+        </View>
+        <View style={calculatorStyle.buttonRow}>
+          <TouchableHighlight onPress={() => operationSelect()}>
+            <Button number="=" color="#f0932b" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleNumberPress("0")}>
+            <Button number="0" color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleNumberPress(".")}>
+            <Button number="." color="#535c68" />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => handleOperation("+")}>
+            <Button number="+" color="#f0932b" />
+          </TouchableHighlight>
+        </View>
+      </View>
     </View>
   );
 }
