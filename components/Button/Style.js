402729e91e0c50c69f777fc2cf94eb01e9e654be
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-07-18 14:29:52 -0300

    style: estiliznado design do botão

--- components/Button/Style.js
+++ components/Button/Style.js
@@ -1,1 +1,16 @@
+import { StyleSheet } from "react-native";
 
+const buttonStyle = StyleSheet.create({
+  container: {
+    backgroundColor: "#535c68",
+    width: 80,
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+  },
+  number: {
+    color: "#fff",
+    fontSize: 20,
+  },
+});
